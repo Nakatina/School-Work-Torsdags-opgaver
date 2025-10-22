07600db77908f44d2b5cb6c2b260c504382da144
Nakatina <qwafel1122@gmail.com>
2025-10-22 09:59:38 +0200

    Laptop endringer
    laptop endringer. ved ikke hvadd

--- Uge 41 Torsdags Opgaver/Torsdags opgave/src/Opgave1/Main.java
+++ Uge 41 Torsdags Opgaver/Torsdags opgave/src/Opgave1/Main.java
@@ -11,7 +11,9 @@ public class Main {
         //fejlen er tydeligt når man kigger på det, Den ligger i navne2 da Arrayed har kun 2 navne og vi prøver at kalde et extra der ikke er der.
         //Derfor har vi i Fejl
 
-        printTreForsteElementer(navne1);
+        ArrayFejl.printTreForsteElementer(navne1);
         printTreForsteElementer(navne2);
+
+
     }
 } //Jeg Kunne nok godt have lavet det her anderledes. Istedet for at Importe, Men jeg tror at der er noget jeg har glemt.
