086b00756fe8c3f09d61827518cec39444c6f1d1
Nakatina <qwafel1122@gmail.com>
2025-11-19 12:58:23 +0100

    Lavet kurv klassen er en WIP
    WIP på kurv

---
+++ Uge 47 Torsdags Opgave/Supermarkede/src/CSVLoader.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CSVLoader {
+
+    public static Map<String, Vare> load(String filename) {
+        Map<String, Vare> varer = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+            String line = br.readLine(); // første linje = header → spring over
+
+            while ((line = br.readLine()) != null) {
+                String[] cols = line.split(";"); // eller "," afhængigt af filen
+
+                // Hent værdierne
+                String ean = cols[0];
+                String navn = cols[1];
+                LocalDate dato = LocalDate.parse(cols[2]);
+                double pris = Double.parseDouble(cols[3]);
+                double maengde = Double.parseDouble(cols[4]);
+                String enhed = cols[5];
+
+                // Lav vare-objektet
+                Vare v = new Vare(ean, navn, dato, pris, maengde, enhed);
+
+                // Læg i map
+                varer.put(ean, v);
+            }
+
+        } catch (IOException e) {
+            System.out.println("FEJL ved indlæsning af CSV: " + filename);
+            e.printStackTrace();
+        }
+
+        return varer;
+    }
+}
---
+++ Uge 47 Torsdags Opgave/Supermarkede/src/Kurv.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kurv {
+    Map<String, Integer> kurv = new HashMap<>();
+
+}
