a62ee33abff42975b86c3efa90e57b659abb78d5
Nakatina <qwafel1122@gmail.com>
2025-11-20 11:19:00 +0100

    kom til at lave redudant kurv klalsse
    skal huske at slætte.

--- Uge 47 Torsdags Opgave/Supermarkede/src/Kurv.java
+++ Uge 47 Torsdags Opgave/Supermarkede/src/Kurv.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
-
+//Tror jeg lavede den her klasse for meget. Slet den når du husker det!
 public class Kurv {
     Map<String, Integer> kurv = new HashMap<>();
 
